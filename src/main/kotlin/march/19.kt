eba829620b6ab605bb2d65f34637f099651620c8
Denis Mironov <denis5918@gmail.com>
2024-03-03 15:15:08 +0700

    Remove Nth Node From End of List, 3.03.24

---
+++ src/main/kotlin/march/19.kt
@@ -0,0 +1,24 @@
+package march
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+    var result: ListNode? = ListNode(0)
+    result?.next = head
+    var fast = result
+    var slow = result
+    repeat(n + 1) {
+        fast = fast?.next
+    }
+
+    while (fast != null) {
+        fast = fast?.next
+        slow = slow?.next
+    }
+
+    slow?.next = slow?.next?.next
+
+    return result
+}
