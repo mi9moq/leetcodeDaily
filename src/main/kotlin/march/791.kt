276fd7aba5fd1e7cce6a4e7aa7326e47d3a02560
Denis Mironov <denis5918@gmail.com>
2024-03-11 22:02:31 +0700

    Custom Sort String, 11.03.24

---
+++ src/main/kotlin/march/791.kt
@@ -0,0 +1,25 @@
+package march
+
+fun main() {
+    customSortString("cba", "abcd")
+}
+
+fun customSortString(order: String, s: String): String {
+    val result = StringBuilder()
+    val builders = Array(26) { StringBuilder() }
+
+    for (c in s) {
+        builders[c - 'a'].append(c)
+    }
+
+    for (c in order) {
+        result.append(builders[c - 'a'])
+        builders[c - 'a'].setLength(0)
+    }
+
+    for (b in builders) {
+        result.append(b)
+    }
+
+    return result.toString()
+}
